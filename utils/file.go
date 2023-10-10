caf8f8bb9f1eeaf65ba6da4a436ffca4ee77401f
Fermin Blanco <luillyfe89@gmail.com>
2023-10-10 17:37:36 -0500

    features: given a json lines file it reads from it

--- utils/file.go
+++ utils/file.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 )
 
+// Given a JSON files it reads from
 func OpenJSONFile(fileName string, Decoder interface{}, textEmdeddings chan interface{}) {
 	// Open the JSON file
 	file, err := os.Open(fileName)
@@ -23,4 +25,25 @@ func OpenJSONFile(fileName string, Decoder interface{}, textEmdeddings chan inte
 
 	// Pass the text to the channel
 	textEmdeddings <- Decoder
+}
+
+// Given a JSON Lines files it reads from
+func OpenJSONLinesFile(fileName string, Decoder interface{}, textEmdeddings chan interface{}) {
+	// Open the JSON Lines file
+	lines, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	// Decode the JSON Lines file
+	data := make([]interface{}, 0)
+	for _, line := range strings.Split(string(lines), "\n") {
+		if err := json.Unmarshal([]byte(line), &Decoder); err != nil {
+			panic(err)
+		}
+		data = append(data, Decoder)
+	}
+
+	// Pass the text to the channel
+	textEmdeddings <- data
 }

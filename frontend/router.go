6ccbda0f0325a481418fe345f0ec5ba6cfdc1c99
Fermin Blanco <luillyfe89@gmail.com>
2023-10-23 21:08:36 -0500

    features: handling queries

--- frontend/router.go
+++ frontend/router.go
@@ -1,16 +1,36 @@
 package frontend
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Run() {
 	// Creating a router
 	router := gin.Default()
 
-	// API Definition
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{"message": "success!"})
-	})
+	// Queries API
+	router.POST("/", postQueries)
 
 	// Run
 	router.Run("localhost:8080")
+}
+
+type Request struct {
+	Query string `form:"query"`
+}
+
+// It handles the search query and returns a semantic similarity search result.
+func postQueries(ctx *gin.Context) {
+	// Will Hold the request data
+	var request Request
+
+	// Decode query text
+	if ctx.ShouldBind(&request) == nil {
+		log.Println(request.Query)
+	}
+
+	// Send back a success message
+	ctx.JSON(200, gin.H{"message": "success!"})
 }

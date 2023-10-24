6ae0ae477bc05892ca62a5c0a81df59f3e08e4de
Fermin Blanco <luillyfe89@gmail.com>
2023-10-24 11:07:49 -0500

    features: processing queries with AI.

--- frontend/router.go
+++ frontend/router.go
@@ -1,9 +1,11 @@
 package frontend
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
+	ai "luillyfe.com/ai/semanticSearch"
 )
 
 func Run() {
@@ -18,7 +20,7 @@ func Run() {
 }
 
 type Request struct {
-	Query string `form:"query"`
+	Query string `form:"query" binding:"required"`
 }
 
 // It handles the search query and returns a semantic similarity search result.
@@ -28,9 +30,22 @@ func postQueries(ctx *gin.Context) {
 
 	// Decode query text
 	if ctx.ShouldBind(&request) == nil {
-		log.Println(request.Query)
+		log.Printf("Query received: %s", request.Query)
+		processQuery(request.Query)
 	}
 
 	// Send back a success message
 	ctx.JSON(200, gin.H{"message": "success!"})
+}
+
+func processQuery(query string) {
+	// Get embedding for the current query
+	featureVector := ai.GetEmbedding(query)
+
+	// Query the index on vector search
+	closestNeighbors := ai.Search(featureVector)
+
+	fmt.Println(closestNeighbors)
+	// Get results from query
+	// return ai.GetResults(closestNeighbors)
 }
